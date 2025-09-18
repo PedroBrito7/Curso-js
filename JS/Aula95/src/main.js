bcb5ebe253787c200b3f1fcd9a0f714b0fecc4ce
Pedro Henrique Brito De Freitas <pedrobritofreitas10@gmail.com>
2025-09-18 18:18:51 -0300

    Criar o gera

--- JS/Aula95/src/main.js
+++ JS/Aula95/src/main.js
@@ -3,7 +3,6 @@ import './assets/css/style.css';
 
 
 (function(){
-    const gera= new GeraCPF();
     const cpfGerado = document.querySelector('.cpf-gerado');
  cpfGerado.innerHTML = gera.geraNovoCpf();
 
