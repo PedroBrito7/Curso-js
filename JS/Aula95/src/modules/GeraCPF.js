c109ff590051c21b979328af3cc9f9564bb5d69d
Pedro Henrique Brito De Freitas <pedrobritofreitas10@gmail.com>
2025-09-19 19:58:53 -0300

    dnv

--- JS/Aula95/src/modules/GeraCPF.js
+++ JS/Aula95/src/modules/GeraCPF.js
@@ -5,6 +5,15 @@ export default class GeraCPF {
  rand(min =  100000000 , max= 999999999){
     return String(Math.floor (math.random() * (max - min )+ min ));
  }
+ formatado(cpf){
+   return (
+      cpf.slice(0,3) + '.',
+      cpf.slice(3,6) + '.',
+      cpf.slice(6,9) + '-',
+      cpf.slice(9,11) 
+);
+
+ }
  GeraNovoCpf(){
     const cpfSemDigitos= this.rand();
     const digito1 = ValidaCPF.geraDigito(cpfSemDigitos);
